1d6a2104ab1ccc2d0eaf1a185ad9b3ccaae100e6
Владимир Тазенков <fadefaze@icloud.com>
2023-06-20 12:25:00 +0300

    add brain-even.js game

---
+++ bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import evenGame from '../src/even-odd_game.js';
+
+evenGame();
---
+++ src/even-odd_game.js
@@ -0,0 +1,42 @@
+import readlineSync from 'readline-sync';
+
+function questionNumber() {
+  return Math.floor(Math.random() * 100);
+}
+function evenCheck(number) {
+  if (number % 2 === 0 && number !== 0) {
+    return true;
+  }
+  return false;
+}
+function evenGame() {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let correctCounter = 1;
+  while (correctCounter > 0) {
+    if (correctCounter === 4) {
+      console.log(`Congratulations! ${userName}`);
+      break;
+    }
+    const question = questionNumber();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    let correctAnswer;
+    if (evenCheck(question)) {
+      correctAnswer = 'yes';
+    } else {
+      correctAnswer = 'no';
+    }
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctCounter += 1;
+    } else {
+      correctCounter = 0;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again ${userName}!`);
+      break;
+    }
+  }
+}
+export default evenGame;
